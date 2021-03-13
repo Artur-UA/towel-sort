f55e8072bbc42bbf95e192e437d171a51e7cc411
Artur <kovalskartur@yahoo.com>
2021-03-13 22:27:18 +0200

    feat : add sort

--- src/index.js
+++ src/index.js
@@ -2,5 +2,7 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
-}
+  if (matrix === undefined) return [];
+  let myarr = matrix.reduce((a,b) => [...a, ...(matrix.indexOf(b) % 2 === 0 ?  b : b.reverse())], []);
+  return myarr;
+};
